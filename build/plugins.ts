0db1c83b28ecbfce21d2d3ae1fca5c3103191d3a
wangjiahui <13226651554@163.com>
2025-07-10 17:20:46 +0800

    fix: 添加配置注释说明

--- build/plugins.ts
+++ build/plugins.ts
@@ -12,18 +12,41 @@ import { isDev } from './util'
 
 export function getPluginsList(env: string): PluginOption[] {
   return [
+    /**
+     * 提供 Vue 3 单文件组件支持
+     */
     vue(),
+    /**
+     * 添加 Vue DevTools 支持
+     * https://devtools-next.vuejs.org/guide/vite-plugin
+     */
     vueDevTools(),
+    /**
+     * 点击页面上的 DOM，它能够自动打开你的 IDE 并将光标定位到 DOM 对应的源代码位置
+     * https://inspector.fe-dev.cn
+     */
     codeInspectorPlugin({
       bundler: 'vite',
       hideConsole: true
     }),
+    /**
+     * UnoCSS  UnoCSS 是一个原子 CSS 引擎，用于将 CSS 转换为原子 CSS
+     * https://unocss.dev
+     */
     UnoCSS(),
+    /**
+     * 按需自动导入 API
+     * https://github.com/unplugin/unplugin-auto-import#readme
+     */
     AutoImport({
       dts: 'src/auto-imports.d.ts',
       resolvers: [ElementPlusResolver()],
       imports: [{ 'element-plus/es': ['ElMessage', 'ElMessageBox', 'ElNotification'] }]
     }),
+    /**
+     * 按需自动导入组件
+     * https://github.com/antfu/unplugin-vue-components#readme
+     */
     Components({
       dts: 'src/auto-components.d.ts',
       resolvers: [
@@ -32,6 +55,11 @@ export function getPluginsList(env: string): PluginOption[] {
         })
       ]
     }),
+    /**
+     * 引入 element-plus 的样式
+     * 在开发环境默认导入全部样式，解决动态识别导致页面白屏时间过长
+     * 在非开发环境，默认导入element-plus命令式组件的必要样式
+     */
     {
       name: 'import-element-plus-style',
       transform(code, id) {
@@ -46,10 +74,17 @@ export function getPluginsList(env: string): PluginOption[] {
         }`
       }
     } satisfies PluginOption,
+    /**
+     * 和import-element-plus-style配合使用
+     * 在非开发环境，默认导入element-plus已使用组件的必要样式
+     */
     ElementPlus({
       useSource: true
     }),
-    // svg组件化支持
+    /**
+     * svg组件化支持 svg-loader
+     * https://github.com/visualfanatic/vite-svg-loader
+     */
     svgLoader()
   ]
 }
--- build/util.ts
+++ build/util.ts
@@ -1,14 +1,37 @@
 import path, { resolve } from 'node:path'
 import process from 'node:process'
 
+/**
+ * Repo root
+ */
 const repoRoot = path.resolve(__dirname, '..')
 
+/**
+ * 获取 packages 包的源代码路径。
+ *
+ * @param pkgName - 包名
+ * @returns 包的源代码路径
+ */
 export function monorepoResolver(pkgName: string) {
   return path.join(repoRoot, 'packages', pkgName, 'src')
 }
 
+/**
+ * 解析路径为绝对路径。
+ * 该函数基于当前工作目录（process.cwd）解析给定目录路径，
+ * 确保返回标准化的绝对路径格式。
+ *
+ * @param dir - 需要解析的相对路径或目录名
+ * @returns 解析后的绝对路径字符串
+ */
 export function pathResolve(dir: string) {
   return resolve(process.cwd(), '.', dir)
 }
 
+/**
+ * 判断当前环境是否为开发环境。
+ *
+ * @param env - 当前环境变量
+ * @returns 如果当前环境是开发环境则返回 true，否则返回 false
+ */
 export const isDev = (env: string) => env === 'development'
